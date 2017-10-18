e52c6972dbb51f7c9ee09b2a04ebd4808ad30729
chenhaoiu <haiou_chen@sina.cn>
2017-10-18 14:57:25 +0800

    支持邮件发送

--- pyemails/model/emails.py
+++ pyemails/model/emails.py
@@ -32,7 +32,7 @@ class Email(object):
         raise 'set mail_host please'
 
     @classmethod
-    def sendmail(cls, from_user,from_password,to_list,subject=u'标题',content=u'邮件内容',format='',file_name='',mail_host=''):
+    def sendmail(cls, from_user,from_password,to_list,subject=u'标题',content=u'邮件内容',format='',file_name='',mail_host='',cc=None):
         '''
         from_user, from_password: 发件人的用户名和密码
         subject 邮件标题
@@ -41,6 +41,7 @@ class Email(object):
         to_list 发送列表，如 ['abc@qq.com','efg@qq.com']
         sender_name 发送者名称
         mail_host 发件人的邮件host, 如 smtp.qq.com
+        cc 抄送给谁, 如 ['abc@qq.com','efg@qq.com']
         '''
         me = from_user + '<' + from_user + '>'
         msg = MIMEMultipart('alternative')
@@ -55,10 +56,15 @@ class Email(object):
             mail_attach["Content-Type"] = 'application/octet-stream'
             mail_attach["Content-Disposition"] = 'attachment; filename="%s"' % (file_name.encode('utf-8'))
             msg.attach(mail_attach)
-
         msg['Subject'] = subject
         msg['From'] = me
-        msg['To'] = ";".join(to_list)
+        if isinstance(to_list, basestring):
+            to_list = [to_list]
+        msg['To'] = ", ".join(to_list)
+        if cc:
+            if isinstance(cc, basestring):
+                cc = [cc]
+            msg['Cc'] = ', '.join(cc)
         try:
             server = smtplib.SMTP()
             if mail_host:
@@ -66,7 +72,7 @@ class Email(object):
             else:
                 server.connect(cls.get_mail_host(from_user))
             server.login(from_user,from_password)
-            server.sendmail(me, to_list, msg.as_string())
+            server.sendmail(me, to_list + cc, msg.as_string())
             server.close()
         except:
             print traceback.format_exc()
